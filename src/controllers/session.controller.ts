f1840a5fd5f1516862022b703d3dbc6a3402de25
Tahsin Alahi <alahitahsin@gmail.com>
2025-06-04 13:35:12 +0600

    feat: added routes for my sessions and resubmit session for tutor

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,8 +1,11 @@
 import sessionsModel from "../models/sessions.model";
+import usersModel from "../models/users.model";
 import createHttpError from "http-errors";
 import { RequestHandler } from "express";
 import { sessionSchema } from "../validations/session.schema";
 import { zodErrorFormat } from "../utils/zodError.utils";
+import { isValidObjectId } from "mongoose";
+import { Status } from "../models/sessions.model";
 
 const createSession: RequestHandler = async (req, res, next) => {
   try {
@@ -24,4 +27,98 @@ const createSession: RequestHandler = async (req, res, next) => {
   }
 };
 
-export default { createSession };
+const mySessions: RequestHandler = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    // TODO: Compare the email with the one in the session with jwt
+
+    if (!email) {
+      throw createHttpError(400, {
+        message: "Email is required",
+        errors: "Email is required",
+      });
+    }
+
+    const results = await usersModel.aggregate([
+      {
+        $match: {
+          email: email,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+        },
+      },
+      {
+        $lookup: {
+          from: "sessions",
+          localField: "_id",
+          foreignField: "tutor",
+          as: "sessions",
+        },
+      },
+      {
+        $project: {
+          sessions: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    if (results.length === 0) {
+      throw createHttpError(404, {
+        message: "Email not found",
+        errors: "Email not found",
+      });
+    }
+
+    if (results[0].sessions.length === 0) {
+      throw createHttpError(404, {
+        message: "No sessions found",
+        errors: "No sessions found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Sessions fetched successfully",
+      success: true,
+      data: results[0],
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const resubmitSession: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // TODO: Compare the email with the one in the session with jwt
+    if (!isValidObjectId(id)) {
+      throw createHttpError(400, {
+        message: "Invalid session id",
+        errors: "Invalid session id",
+      });
+    }
+
+    const session = await sessionsModel.findById(id);
+    if (!session) {
+      throw createHttpError(404, {
+        message: "Session not found",
+        errors: "Session not found",
+      });
+    }
+
+    session.status = Status.PENDING;
+    await session.save();
+
+    res.status(200).json({
+      message: "Session resubmitted successfully",
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { createSession, mySessions, resubmitSession };
--- src/routes/session.route.ts
+++ src/routes/session.route.ts
@@ -3,6 +3,7 @@ import sessionController from "../controllers/session.controller";
 
 const router = Router();
 
+router.get("/mine", sessionController.mySessions);
+router.patch("/:id/resubmit", sessionController.resubmitSession);
 router.route("/").post(sessionController.createSession);
-
 export default router;
