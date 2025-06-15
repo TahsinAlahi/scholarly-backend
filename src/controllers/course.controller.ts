2d2ed018b7d579caab359d1d7d0f230717d4f965
Tahsin Alahi <alahitahsin@gmail.com>
2025-06-15 18:42:55 +0600

    feat: get course and delete, also update status

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from "express";
 import { courseSchema } from "../validations/course.schema";
 import createHttpError from "http-errors";
-import { zodErrorFormat } from "../utils";
+import { validateObjectIdOrThrow, zodErrorFormat } from "../utils";
 import { coursesModel } from "../models";
 import { Status } from "../models";
 
@@ -61,4 +61,117 @@ const getMyCourses: RequestHandler = async (req, res, next) => {
   }
 };
 
-export default { createCourse, getMyCourses, getCourses };
+const getCourseDetails: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    validateObjectIdOrThrow(id, "Course");
+
+    const course = await coursesModel.findById(id);
+    if (!course) {
+      throw createHttpError(404, {
+        message: "Course not found",
+        errors: "Course not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Course details fetched successfully",
+      success: true,
+      data: course,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteCourse: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    validateObjectIdOrThrow(id, "Course");
+
+    const course = await coursesModel.findByIdAndDelete(id);
+    if (!course) {
+      throw createHttpError(404, {
+        message: "Course not found",
+        errors: "Course not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Course deleted successfully",
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getPendingCourses: RequestHandler = async (_req, res, next) => {
+  try {
+    const courses = await coursesModel
+      .find({ status: Status.PENDING })
+      .populate({
+        path: "tutor",
+        select: "name email _id",
+      })
+      .select("-__v");
+
+    res.status(200).json({
+      message: "Courses fetched successfully",
+      success: true,
+      data: courses,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateCourseStatus: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    validateObjectIdOrThrow(id, "Course");
+
+    const course = await coursesModel.findById(id);
+    if (!course) {
+      throw createHttpError(404, {
+        message: "Course not found",
+        errors: "Course not found",
+      });
+    }
+
+    const { status, rejectionReason } = req.body;
+    if (!status) {
+      throw createHttpError(400, {
+        message: "Status is required",
+        errors: "Status is required",
+      });
+    }
+    if (!Object.values(Status).includes(status)) {
+      throw createHttpError(400, {
+        message: "Status must be one of: pending, accepted, or rejected",
+        errors: "Unknown status",
+      });
+    }
+    course.status = status;
+    course.rejectionReason = rejectionReason;
+    await course.save();
+
+    res.status(200).json({
+      message: "Course status updated successfully",
+      success: true,
+      data: course,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  createCourse,
+  getMyCourses,
+  getCourses,
+  getCourseDetails,
+  deleteCourse,
+  getPendingCourses,
+  updateCourseStatus,
+};
--- src/routes/course.route.ts
+++ src/routes/course.route.ts
@@ -3,11 +3,17 @@ import courseController from "../controllers/course.controller";
 
 const router = Router();
 
+// TODO: remove the param
+router.get("/mine/:id", courseController.getMyCourses);
+router.get("/pending", courseController.getPendingCourses);
+router.patch("/:id/status", courseController.updateCourseStatus);
+router
+  .route("/:id")
+  .get(courseController.getCourseDetails)
+  .delete(courseController.deleteCourse);
 router
   .route("/")
   .post(courseController.createCourse)
   .get(courseController.getCourses);
-// TODO: remove the param
-router.get("/mine/:id", courseController.getMyCourses);
 
 export default router;
